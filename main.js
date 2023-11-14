c399b94e1bab0877e9bb04b4e3f5c9c4f8ae8fb4
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2023-11-14 11:52:35 +0200

    Attribute binding

---
+++ main.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+    data: function () {
+        return {
+            product: 'Socks',
+            description: 'A warm fuzzy pair of socks.',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://www.vuemastery.com/'
+        }
+    }
+})
