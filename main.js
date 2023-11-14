29f617a5d5546c41485afff4a75987758cd29ed6
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2023-11-14 12:57:03 +0200

    List rendering

--- main.js
+++ main.js
@@ -4,8 +4,14 @@ const app = Vue.createApp({
             product: 'Socks',
             description: 'A warm fuzzy pair of socks.',
             image: './assets/images/socks_green.jpg',
+            details: ['50% cotton', '30% wool', '20% polyester'],
             inventory: 100,
             onSale: true,
+            variants: [
+                { id: 2234, color: 'green'},
+                { id: 2233, color: 'blue'},
+            ],
+            sizes: ['S', 'M', 'L', 'XL'],
             url: 'https://www.vuemastery.com/'
         }
     }
